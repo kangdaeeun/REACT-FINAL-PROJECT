5f4dde435dd2271acad046521b4ef633a6972328
Daeeun Kang <daeeun123@daum.net>
2025-01-02 12:00:01 +0900

    feat:layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,32 @@
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate();
   return (
-    <div>Header</div>
-  )
-}
+    <section>
+      <nav className="flex justify-between">
+        <span>
+          <button onClick={() => navigate("/")}>
+            <img src="1.png">1</img>
+          </button>
+        </span>
+        <span>
+          Mypage
+          {/* {!user ? (
+            <>
+              <button onClick={() => navigate("/login")}>로그인</button>
+              <button onClick={() => navigate("/signup")}>회원가입</button>
+            </>
+          ) : (
+            <>
+              <button onClick={() => navigate("/mypage")}>마이페이지</button>
+              <button onClick={() => navigate("/")}>로그아웃</button>
+            </>
+          )} */}
+        </span>
+      </nav>
+    </section>
+  );
+};
 
-export default Header
+export default Header;
