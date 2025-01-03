96d88a7bf402e5aed6c365c6bcf79ff318abc68a
Daeeun Kang <daeeun123@daum.net>
2025-01-03 18:05:24 +0900

    feat:signup 다듬기

--- README.md
+++ README.md
@@ -18,4 +18,24 @@ tailwind.config.js 에 extend:{} 안에 colors:{} 안에 내가 사용할 색 �
 
 Day 3 SignUp
 참고 : 베이직반 8회차 (2024/11/18) - 회원가입
+트러블 슈팅
+회원가입은 됐는데 supabase에 유저 데이터가 안 들어감
+supabase로 회원가입 할 때 닉네임도 사용해야할 때 즉, 아이디와 비밀번호 외에 추가할 때 로직을 검색해서 찾았음
+const { data, error } = await supabase.auth.signUp(
+      {
+        email,
+        password,
+      },
+      {
+        data: { nickname },
+      }
+    );
+외국인이 작성한 로직이었는데 자세히 보니 However, there is not documentation about it. 라고... 
+튜터님의 라이브 코딩으로 공식문서에 있는 방법으로 고침 공식문서 짱!
 
+회원가입 기능을 구현했는데 오류가 뜸 supabase에 users 오타로 인해 undefind 가 뜨다가 고쳤더니 다시
+null value in column "img_url" of relation "users" violates not-null constraint
+이런 오류가 뜸 유저스에 이미지 콜롬이 널 값이기 때문에 안된다 라는 것 하지만 회원가입시 이미지를 넣지 않기 때문에 supabase설정에서 널 값을 허용토록 변경해야한다
+supabase public-users 콜롬 설정에서 Allow Nullable 체크를 안해서 이런 오류가 떴었음 -> 체크를 해서 이미지 값이 null 이어도 가능함을 설정해주었음
+
+css width, height 값을 정할 때 부모태그에서 너비를 지정해야 그 안에서 조절할 수 있다는 걸 아는데 잘 안돼서 문제점을 찾아보니 tailwind는 공식문서에 나와있는 값만 사용할 수 있음 공식문서에 없는 값을 사용하고 싶을 땐 대괄호[]를 사용해서 그 안에 값을 넣어서 사용하면 된다.!!!! ex w-[500px]
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { ChangeEvent, FormEvent, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import supabase from "../utils/supabase";
 
 const Signup = () => {
@@ -9,102 +9,145 @@ const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [checkPassword, setCheckPassword] = useState("");
-  const [user, setUser] = useState(null);
 
-  const onChangeEmail = (e) => {
+  const onChangeEmail = (e: ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value);
   };
 
-  const onChangePassword = (e) => {
+  const onChangePassword = (e: ChangeEvent<HTMLInputElement>) => {
     setPassword(e.target.value);
   };
 
-  const onChangeNickname = (e) => {
+  const onChangeNickname = (e: ChangeEvent<HTMLInputElement>) => {
     setNickname(e.target.value);
   };
 
-  const onChangeCheckPassword = (e) => {
+  const onChangeCheckPassword = (e: ChangeEvent<HTMLInputElement>) => {
     setCheckPassword(e.target.value);
   };
 
-  const signUpNewUser = async (e) => {
+  const signUpNewUser = async (e: FormEvent<HTMLFormElement>) => {
     // 새로고침 방지
     e.preventDefault();
 
     // 유효성 검사
-    const emailReg = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    if (!emailReg.test(email)) {
-      alert("이메일 형식이 올바르지 않습니다");
-    }
-
-    if (nickname === "") {
+    if (!nickname) {
       alert("닉네임을 입력해주세요");
       return;
     }
 
-    if (password === "") {
-      alert("비밀번호를 입력해주세요");
+    if (nickname.length <= 2) {
+      alert("닉네임은 3글자 이상이어야 합니다");
       return;
     }
 
-    if (checkPassword === "") {
-      alert("비밀번호 확인을 입력해주세요");
+    const emailReg = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!emailReg.test(email)) {
+      alert("이메일 형식이 올바르지 않습니다");
       return;
     }
 
-    const { data, error } = await supabase.auth.signUp(
-      {
-        email,
-        password,
+    if (password.length < 8) {
+      alert("비밀번호는 8자 이상이어야 합니다");
+      return;
+    }
+
+    if (password !== checkPassword) {
+      alert("비밀번호가 일치하지 않습니다");
+      return;
+    }
+
+    // 1. supabase 회원가입
+    const { data, error } = await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        data: {
+          nickname,
+        },
       },
-      {
-        data: { nickname },
-      }
-    );
-    console.log("signup: ", { data, error });
-    setUser(data.user);
+    });
 
-    setNickname("");
-    setEmail("");
-    setPassword("");
-    setCheckPassword("");
-  };
-
-  const onSignup = () => {
-    if(signUpNewUser()) {
-      alert('회원가입이 완료되었습니다')
+    if (error) {
+      alert(`회원가입에 실패했습니다. ${error.message}`);
+      return;
     }
+
+    // 2. users 테이블에도 넣어준다
+    const { error: userError } = await supabase.from("users").insert({
+      id: data.user?.id,
+      email: email,
+      nickname: nickname,
+    });
+
+    if (userError) {
+      alert(`유저 생성에 실패했습니다. ${userError.message}`);
+      return;
+    }
+
+    alert("회원가입에 성공했습니다");
     navigate("/");
   };
 
   return (
-    <form onSubmit={signUpNewUser}>
-      <input
-        type="text"
-        placeholder="닉네임"
-        value={nickname}
-        onChange={onChangeNickname}
-      />
-      <input
-        type="text"
-        placeholder="이메일"
-        value={email}
-        onChange={onChangeEmail}
-      />
-      <input
-        type="text"
-        placeholder="비밀번호"
-        value={password}
-        onChange={onChangePassword}
-      />
-      <input
-        type="text"
-        placeholder="비밀번호 확인"
-        value={checkPassword}
-        onChange={onChangeCheckPassword}
-      />
-      <button onClick={onSignup}>회원가입</button>
-    </form>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="text-2xl font-bold">회원가입</div>
+      <div className="p-1 m-1 w-3/4 h-2/3 border-2 border-solid rounded border-selected-white">
+        <form
+          onSubmit={signUpNewUser}
+          className="flex items-center justify-center w-full h-full"
+        >
+          <div className="flex flex-col gap-2 w-[300px]">
+            <input
+              className="border-2 border-gray-mint rounded-md p-2 hover:border-black-blue hover:border-2"
+              type="text"
+              placeholder="닉네임"
+              value={nickname}
+              onChange={onChangeNickname}
+              required
+            />
+            <input
+              className="border-2 border-gray-mint rounded-md p-2 hover:border-black-blue hover:border-2"
+              type="text"
+              placeholder="이메일"
+              value={email}
+              onChange={onChangeEmail}
+              required
+            />
+            <input
+              className="border-2 border-gray-mint rounded-md p-2 hover:border-black-blue hover:border-2"
+              type="password"
+              placeholder="비밀번호"
+              value={password}
+              onChange={onChangePassword}
+              required
+            />
+            <input
+              className="border-2 border-gray-mint rounded-md p-2 hover:border-black-blue hover:border-2"
+              type="password"
+              placeholder="비밀번호 확인"
+              value={checkPassword}
+              onChange={onChangeCheckPassword}
+              required
+            />
+            <p className="flex justify-around">
+              <button
+                className="className=py-2 px-4 bg-blue-400 text-white rounded-md text-black hover:bg-selected-white"
+                type="submit"
+              >
+                회원가입
+              </button>
+              <Link
+                to="/"
+                className="className=py-2 px-4 bg-gray-mint text-white rounded-md text-black hover:bg-selected-white"
+              >
+                뒤로 가기
+              </Link>
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 };
 
